b5e761b461e52170bb789534f4150191d0564920
KshipraKode <kshiprakode@gmail.com>
2016-04-20 01:34:49 -0400

    Sync, Login, Splashscreen UI

--- app/src/main/java/com/example/soundless/MainActivity.java
+++ app/src/main/java/com/example/soundless/MainActivity.java
@@ -1,31 +1,22 @@
 package com.example.soundless;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button syncButton = (Button) findViewById(R.id.button3);
+        syncButton.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +39,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId())
+        {
+            case R.id.button3:
+                Intent intent = new Intent(this,Sync.class);
+                startActivity(intent);
+        }
     }
 }
