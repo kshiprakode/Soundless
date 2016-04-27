80658efc64575974011593361a99db93b3bf72a2
KshipraKode <kshiprakode@gmail.com>
2016-04-27 12:37:41 -0400

    Updated Changes

---
+++ app/src/main/java/com/example/soundless/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.soundless;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    int SPLASH_SCREEN_TIMEOUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
